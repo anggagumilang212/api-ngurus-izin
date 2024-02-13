61f01a9d5abf43e6a041f786bf7251387a54ec2e
zhoriefzainulmuttaqin <zhoriefzainulmuttaqin@gmail.com>
2024-02-13 13:42:41 +0700

    improve api

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ db.sequelize.sync()
   });
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://localhost:3000"
 };
 
 app.use(cors(corsOptions));
@@ -42,7 +42,7 @@ require("./app/routes/administrators")(app);
 require("./app/routes/index")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
