47e83b2fcfbff9f638eae85faa02aaa0464fbd56
anggagumilang212 <anggagumilang212@gmail.com>
2024-03-20 23:52:07 +0700

    cord

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const cors = require("cors");
-const db = require("./app/models");
 
 const app = express();
+app.use("/layanan", express.static("public/assets/images/layanan")); //masukkan public direktori
+app.use("/transaksi", express.static("public/assets/images/transaksi")); //masukkan public direktori
+app.use("/tentang", express.static("public/assets/images/tentang")); //masukkan public direktori
+app.use("/testimoni", express.static("public/assets/images/testimoni")); //masukkan public direktori
+app.use(cors());
 
-// Serve static files
-app.use("/layanan", express.static("public/assets/images/layanan"));
-app.use("/transaksi", express.static("public/assets/images/transaksi"));
-app.use("/tentang", express.static("public/assets/images/tentang"));
-app.use("/testimoni", express.static("public/assets/images/testimoni"));
-
-// Sync database
+const db = require("./app/models");
 db.sequelize
   .sync()
   .then(() => {
@@ -20,26 +18,23 @@ db.sequelize
     console.log("Failed to sync db: " + err.message);
   });
 
-// Parse requests of content-type - application/json
-app.use(express.json());
-
-// Parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
-
-// Set CORS options to allow all origins
-const corsOptions = {
-  origin: "*",
+var corsOptions = {
+  origin: "https://ngurusizin.online",
 };
 
-// Use CORS middleware with the provided configuration
 app.use(cors(corsOptions));
 
-// Simple route
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Izin Aja application." });
 });
 
-// Include routes
 require("./app/routes/tentang")(app);
 require("./app/routes/layanan")(app);
 require("./app/routes/transaksi")(app);
@@ -48,7 +43,7 @@ require("./app/routes/administrators")(app);
 require("./app/routes/auth")(app);
 require("./app/routes/order")(app);
 
-// Set port, listen for requests
+// set port, listen for requests
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
